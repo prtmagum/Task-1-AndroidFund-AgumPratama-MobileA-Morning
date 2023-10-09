40762cb453f8a2cc42bceaa8cac9937a63a2c96e
Agum Pratama <pratamaagum15@gmail.com>
2023-10-09 19:31:30 +0700

    Agum Pratama Mobile Morning A

---
+++ app/src/main/java/com/example/latihan/Fragment1.kt
@@ -0,0 +1,6 @@
+package com.example.latihan
+
+import androidx.fragment.app.Fragment
+
+class Fragment1 :Fragment(R.layout.fragment1) {
+}
---
+++ app/src/main/java/com/example/latihan/Fragment2.kt
@@ -0,0 +1,6 @@
+package com.example.latihan
+
+import androidx.fragment.app.Fragment
+
+class Fragment2 : Fragment(R.layout.fragment2) {
+}
